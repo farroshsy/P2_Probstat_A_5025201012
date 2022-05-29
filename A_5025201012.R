0bf14c6857476c1acfc799b14d2d68a0987e722b
Farros <86004023+farroshsy@users.noreply.github.com>
2022-05-29 19:31:45 +0700

    Update A_5025201012.R

--- A_5025201012.R
+++ A_5025201012.R
@@ -30,64 +30,61 @@ sd_after
 # Menggunakan t-test
 t.test(before, after, alternative = "greater", var.equal = FALSE)
 
-# 1c
-# tentukanlah apakah terdapat pengaruh yang signifikan secara statistika
-# dalam hal kadar saturasi oksigen , sebelum dan sesudah melakukan
-# aktivitas ???? jika diketahui tingkat signifikansi ???? = 5% serta H0 : "tidak ada
-# pengaruh yang signifikan secara statistika dalam hal kadar saturasi
-# oksigen , sebelum dan sesudah melakukan aktivitas ????"
+# Poin 1c
+# tentukanlah apakah terdapat pengaruh yang signifikan secara statistika dalam hal kadar saturasi oksigen , sebelum dan sesudah melakukan aktivitas A jika diketahui tingkat signifikansi a = 5% serta H0 : "tidak ada pengaruh yang signifikan secara statistika dalam hal kadar saturasi oksigen, sebelum dan sesudah melakukan aktivitas A
 
 var.test(before, after)
 
 t.test(before, after, mu = 0, alternative = "two.sided", var.equal = TRUE)
 
+# Poin 2a
 
-# Soal 2
 install.packages("BSDA")
 library(BSDA)
 
-# Soal 2a
-# Jawaban di README
+#Setuju
 
-# Soal 2b 
+# Poin 2b 
+
 tsum.test(mean.x=23500, sd(3900), n.x=100)
 
-# Soal 2c
-# Jawaban di README
+# Poin 2c
+# Kesimpulan yang didapat yaitu dapat diketahui bahwa mobil dikemudikan rata-rata lebih dari 20.000 kilometer per tahun
 
-# Soal 3
-# Soal 3a
+
+# Poin 3a
 # H0 dan H1
-# Jawaban di README
+# H0 = 9.50 && H1 = 10.98
 
-# soal 3b 
+
+# Poin 3b 
 # Hitung Sampel Statistik
+
 tsum.test(mean.x=3.64, s.x = 1.67, n.x = 19, 
           mean.y =2.79 , s.y = 1.32, n.y = 27, 
           alternative = "greater", var.equal = TRUE)
 
-# soal 3c
-# Lakukan Uji Statistik (df =2)
+# Poin 3c
+# Lakukan Uji Statistik (df = 2)
+
 install.packages("mosaic")
 library(mosaic)
 
-plotDist(dist='t', df=2, col="blue")
+plotDist(dist='t', df=2, col="dark blue")
 
-# soal 3d
+# Poin 3d
 # Nilai Kritikal
+
 qchisq(p = 0.05, df = 2, lower.tail=FALSE)
 
-# soal 3e
-# Jawaban di README
+# Poin 3e
+# Teori keputusan adalah teori formal pengambilan keputusan di bawah ketidakpastian. 
 
-# soal 3f
-# Jawaban di README
+# Poin 3f
+# Kesimpulan yang didapatkan yaitu perbedaan rata-rata yang terjadi tidak ada jika dilihat dari uji statistik dan akan ada tetapi tidak signifikan jika dipengaruhi nilai kritikal.
 
-# Soal 4
-# Soal 4a
-# Buatlah masing masing jenis spesies menjadi 3 subjek "Grup" (grup 1,grup
-# 2,grup 3). Lalu Gambarkan plot kuantil normal untuk setiap kelompok dan
-# lihat apakah ada outlier utama dalam homogenitas varians.
+# Poin 4a
+# Buatlah masing masing jenis spesies menjadi 3 subjek "Grup" (grup 1, grup 2, grup 3). Lalu Gambarkan plot kuantil normal untuk setiap kelompok dan lihat apakah ada outlier utama dalam homogenitas varians.
 
 myFile  <- read.table(url("https://rstatisticsandresearch.weebly.com/uploads/1/0/2/6/1026585/onewayanova.txt"))
 dim(myFile)
@@ -103,21 +100,21 @@ group1 <- subset(myFile, V1=="Kucing Oren")
 group2 <- subset(myFile, V1=="Kucing Hitam")
 group3 <- subset(myFile, V1=="Kucing Putih")
 
-# Soal 4b
-# carilah atau periksalah Homogeneity of variances nya , Berapa nilai p yang
-# didapatkan? , Apa hipotesis dan kesimpulan yang dapat diambil ?
+# Poin 4b
+# carilah atau periksalah Homogeneity of variances nya , Berapa nilai p yang didapatkan? , Apa hipotesis dan kesimpulan yang dapat diambil ?
 bartlett.test(Length~V1, data=dataoneway)
 
-# Soal 4c
-# Untuk uji ANOVA (satu arah), buatlah model linier dengan Panjang versus
-# Grup dan beri nama model tersebut model 1.
+# Poin 4c
+# Untuk uji ANOVA (satu arah), buatlah model linier dengan Panjang versus Grup dan beri nama model tersebut model 1.
 qqnorm(group1$Length)
 qqline(group1$Length)
 
-# Soal 4d
-# Jawaban di README
+# Poin 4d
+# Didapatkan nilai dari p-value yaitu = 0.8054. 
 
-# Soal 4e
+# Poin 4e
+# Verifikasilah model 1 dengan Post-hoc test Tukey HSD
+
 model1 <- lm(Length~Group, data=myFile)
 
 anova(model1)
@@ -125,13 +122,16 @@ anova(model1)
 TukeyHSD(aov(model1))
 
 # Soal 4f
+# Visualisasikan data dengan ggplot2
+
 library(ggplot2)
 ggplot(dataoneway, aes(x = Group, y = Length)) + geom_boxplot(fill = "grey80", colour = "black") + 
   scale_x_discrete() + xlab("Treatment Group") +  ylab("Length (cm)")
 
 
-# Soal 5
-# Soal 5a
+# Poin 5a
+# Buatlah plot sederhana untuk visualisasi data
+
 install.packages("multcompView")
 library(readr)
 library(ggplot2)
@@ -146,7 +146,9 @@ str(GTL)
 qplot(x = Temp, y = Light, geom = "point", data = GTL) +
   facet_grid(.~Glass, labeller = label_both)
 
-# Soal 5b
+# Poin 5b
+# Uji ANOVA dua arah
+
 GTL$Glass <- as.factor(GTL$Glass)
 GTL$Temp_Factor <- as.factor(GTL$Temp)
 str(GTL)
@@ -154,17 +156,25 @@ str(GTL)
 anova <- aov(Light ~ Glass*Temp_Factor, data = GTL)
 summary(anova)
 
-# Soal 5c
+# Poin 5c
+# Tabel dengan mean dan standar deviasi keluaran cahaya untuk setiap perlakuan
+
 data_summary <- group_by(GTL, Glass, Temp) %>%
   summarise(mean=mean(Light), sd=sd(Light)) %>%
   arrange(desc(mean))
 print(data_summary)
 
-# Soal 5d
+# Poin 5d
+# Uji Tukey
+
 tukey <- TukeyHSD(anova)
 print(tukey)
 
-# Soal 5e
+# Poin 5e
+# Compact letter display untuk menunjukkan perbedaan signifikan
+
+antara uji Anova dan uji Tukey
+
 tukey.cld <- multcompLetters4(anova, tukey)
 print(tukey.cld)
 
